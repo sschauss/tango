e68c2a89875fdce1bdcbc3cd78ab23a827f2d64e
Simon Schauß <simon.schauss@gmail.com>
2016-11-02 06:42:38 +0000

    [ass01] task 3 home

--- assignment1/tango_assignment1_4.py
+++ assignment1/tango_assignment1_4.py
@@ -1,29 +1,16 @@
 
-# coding: utf-8
-
 # Maps the values of an empty array of length 10 to a random number between 0 and 90.
-
-# In[ ]:
-
 import random
 
 randlist = list(map(lambda x: random.randint(0,90), [None] * 10))
 
-
 # Maps the previous generated random numbers to their sine and cosine values.
-
-# In[ ]:
-
 import math
 
 sin = list(map(math.sin, randlist))
 cos = list(map(math.cos, randlist))
 
-
 # First plot the values of the sin and cos list, set the line type to dots and define the labels. Then the legend for the labels is created. After that the viewport is defined. Finally the plot is rendered.
-
-# In[ ]:
-
 import matplotlib.pyplot as plt
 
 plt.plot(sin, "o", label="sine")
@@ -33,4 +20,3 @@ plt.legend(loc=1, borderaxespad=0, numpoints=1)
 plt.xlim([-1, 14])
 plt.ylim([-5/4, 5/4])
 plt.show()
-
