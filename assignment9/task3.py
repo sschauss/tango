cdfdf83b2b5fd104cfce332e903bf24cf798b25f
Lukas Härtel <lukashaertel@gmail.com>
2017-01-20 16:02:11 +0100

    Replaced the slow version with something better

--- assignment9/task3.py
+++ assignment9/task3.py
@@ -1,3 +1,6 @@
+from tqdm import tqdm
+
+
 def read_input():
     """
     Reads the input data and returns a matrix representing
@@ -22,88 +25,123 @@ def read_input():
         index += 1
 
     # Create the matrix
-    result = dict()  # parseMatrix()
+    result = dict()
 
     # Initialize the matrix
-    for node, row in dataset.iterrows():
+    for node, row in tqdm(dataset.iterrows(),
+                          total=dataset.size,
+                          desc="Preparing matrix",
+                          unit="row"):
+        # Map first node
         n1 = fwd.get(node)
-        result[n1, n1] = 0
         for edge in row.out_links:
+            # Map second node
             n2 = fwd.get(edge)
             if n2 is not None:
-                result[n1, n2] = 1
 
+                # Add a connection, create a new row if not present
+                row = result.get(n1)
+                if row is None:
+                    row = {n2: 1}
+                    result[n1] = row
+                else:
+                    row[n2] = 1
+
+    # Return the matrix and the node mapping rules
     return result, fwd, bwd
 
 
-def all_pairs_shortest_path(matrix, vs, inf=999999999):
+def dijkstra(matrix, progress, allocated, s):
     """
-    Updates all pairs shortest path using the Floyd-Warshall algorithm
-    :param matrix: The matrix of distance values to compute in, assumed
-    to contain m(x, x)=0.
-    :param vs: The domain of vertices
-    :param inf: The value to use for unconnected nodes, needs to be
-    greater than every potential path
-    :return: Returns None
+    Calculates Dijkstra and reports to a common progress bar.
+    :param matrix: The matrix representation of the graph
+    :param progress: The Progress to report to
+    :param allocated: The allocated maximum units of work
+    :param s: The node to start from
+    :return: Returns the distance dictionary for node s
     """
-    from time import sleep, time
+    from heapq import heappush, heappop
+
+    # Value greater than everything else, not necessarily infinity
+    inf = len(matrix) + 1
+
+    # Initialize result vector and queue
+    res = {}
+    queue = []
+
+    # Set initial distances
+    for v in matrix:
+        res[v] = inf
+    res[s] = 0
+
+    heappush(queue, (res[s], s))
+
+    # Maximum units of work
+    while queue:
+        d, u = heappop(queue)
+
+        if d < res[u]:
+            res[u] = d
+
+        for v in matrix[u]:
+            x = res[u] + matrix[u][v]
+            if res[v] > x:
+                res[v] = x
+                heappush(queue, (x, v))
+
+        # Update one, we might finish earlier
+        progress.update()
+        allocated -= 1
+
+    # Update the remaining ticks
+    progress.update(allocated)
+
+    return res
+
+
+def diameter(matrix):
+    """
+    The diameter is the longest shortest path in the graph. Dijkstra returns a
+    map from node to distance. The maximum value is the longest shortest path
+    for one node. The maximum off this value over all nodes returns the diameter
+    of the graph.
+    :param matrix: The matrix representing the graph.
+    :return: Returns the diameter as an integer.
+    """
+
     from concurrent.futures import ThreadPoolExecutor
+    from functools import partial
+    from math import log
 
-    def probe_hop(mid):
-        """
-        For a fixed middle mode probes all hops
-        :param mid: The middle node to probe
-        :return: Returns None
-        """
-        for source in vs:
-            to_mid = matrix.get((source, mid), inf)
-            for target in vs:
-                direct = matrix.get((source, target), inf)
-                from_mid = matrix.get((mid, target), inf)
-                if direct > to_mid + from_mid:
-                    matrix[source, target] = to_mid + from_mid
+    capital_e = sum(len(v) for v in matrix.values())
+    capital_v = len(matrix)
+    worst_case = int(capital_e + capital_v * log(capital_v))
 
-    # Use a thread pool executor for independent variables
-    with ThreadPoolExecutor() as executor:
-        # Submit all vertices to middle hop probe
-        futures = [executor.submit(probe_hop, mid) for mid in vs]
+    # Execute on a thread pool
+    with ThreadPoolExecutor(
+            thread_name_prefix="diam_worker",
+            max_workers=8) as executor:
+        # Report using TQDM
+        with tqdm(
+                desc="APSP calculation",
+                total=len(matrix) * worst_case) as progress:
+            # Bind the unit of work
+            uow = partial(dijkstra, matrix, progress, worst_case)
 
-        # Wait and print status every some seconds
-        dispatched = len(futures)
-        completed = 0
-        started = time()
-        while dispatched > completed:
-            # Get some statistics
-            completed = sum(1 for x in futures if x.done())
-            working = sum(1 for x in futures if x.running())
-            percentage = (100. * completed / dispatched)
-            duration = time() - started
-
-            # Print data
-            print("%d/%d hops probed (%.2f%%), %d working (%.1fs)"
-                  % (completed, dispatched, percentage, working, duration))
-
-            # Sleep for 5 seconds
-            sleep(5)
+            return max(max(x.values())
+                       for x in executor.map(uow, matrix.keys()))
 
 
 def task3():
     """
-    Reads link data, calculates all pairs shortest path and selects the longest shortest path
+    Reads link data, calculates all pairs shortest path and selects the longest
+    shortest path.
     :return: Returns None
     """
 
     # Read the data and the mapping
-    matrix, fwd, _ = read_input()
-
-    # Find all pairs' shortest path
-    all_pairs_shortest_path(matrix, fwd.values())
-
-    # Since all paths are the shortest, the maximum value is the longest shortest path
-    diameter = max(matrix.values())
-
-    # Print the output
-    print("The diameter is %d" % diameter)
+    matrix, _, _ = read_input()
+    print(diameter(matrix))
 
 
 if __name__ == "__main__":
